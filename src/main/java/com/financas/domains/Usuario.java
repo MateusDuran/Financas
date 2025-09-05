7ffe3fbbd3b2cc42a0b72f2a3672f63a5d51f850
Ximenes1910 <pedro.silva1040@hotmail.com>
2025-09-05 15:20:21 -0300

    O Xixi não para mossoooooo

--- src/main/java/com/financas/domains/Usuario.java
+++ src/main/java/com/financas/domains/Usuario.java
@@ -1,6 +1,10 @@
 package com.financas.domains;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Objects;
 
 @Entity
 @Table(name="usuario")
@@ -9,4 +13,47 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_usuario")
     private int idUsuario;
+
+    @NotBlank(message = "O campo Razão Social não pode ser nulo")
+    @NotNull(message = "O campo Razão Social não pode estar vazio")
+    private String razaoSocial;
+
+
+    public Usuario() {
+    }
+
+    public Usuario(int idUsuario, String razaoSocial) {
+        this.idUsuario = idUsuario;
+        this.razaoSocial = razaoSocial;
+    }
+
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return idUsuario == usuario.idUsuario && Objects.equals(razaoSocial, usuario.razaoSocial);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idUsuario, razaoSocial);
+    }
 }
--- src/main/java/com/financas/domains/dtos/UsuarioDTO.java
+++ src/main/java/com/financas/domains/dtos/UsuarioDTO.java
@@ -1,4 +1,38 @@
 package com.financas.domains.dtos;
 
+import com.financas.domains.Usuario;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class UsuarioDTO {
+
+    private int idUsuario;
+
+    @NotBlank(message = "O campo Razão Social não pode ser nulo")
+    @NotNull(message = "O campo Razão Social não pode estar vazio")
+    private String razaoSocial;
+
+    public UsuarioDTO() {
+    }
+
+    public UsuarioDTO(Usuario usuario) {
+        this.idUsuario = usuario.getIdUsuario();
+        this.razaoSocial = usuario.getRazaoSocial();
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
 }
